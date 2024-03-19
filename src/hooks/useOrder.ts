fee376918233fb80b808be8566699a84ac3dbba0
Nestor Lopez <n3zt0r77@gmail.com>
2024-03-18 19:06:59 -0600

    Header, menu and custome Hooks created

---
+++ src/hooks/useOrder.ts
@@ -0,0 +1,15 @@
+import { useState } from "react";
+import type { MenuItem, OrderItem } from "../types";
+
+export default function useOrder() {
+  const [order, setOrder] = useState<OrderItem[]>([]);
+
+  const addItem = (item: MenuItem) => {
+    const newItem = { ...item, quantity: +1 };
+    setOrder([...order, newItem]);
+  };
+
+  console.log(order);
+
+  return { addItem };
+}
